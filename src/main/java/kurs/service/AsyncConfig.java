698fdc7c6481280668bdda8d2d45dfdb10996f72
michal <ttt.ttt2347@gmail.com>
2024-07-07 18:49:30 +0200

    Mikhail_Yarashevich_TEST_2_ROZSZERZENIE_4

---
+++ src/main/java/kurs/service/AsyncConfig.java
@@ -0,0 +1,24 @@
+package kurs.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "figuresImportExecutor")
+    public Executor figuresImportExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("FiguresImport-");
+        executor.initialize();
+        return executor;
+    }
+}
